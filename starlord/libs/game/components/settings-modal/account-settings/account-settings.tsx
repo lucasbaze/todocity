df054d2d7779a5c39c8fb13a35b922d22cf2c287
lucas <lucas.bazemore@republic.co>
2022-09-02 11:47:42 -0600

    add account details

--- starlord/libs/game/components/settings-modal/account-settings/account-settings.tsx
+++ starlord/libs/game/components/settings-modal/account-settings/account-settings.tsx
@@ -1,10 +1,16 @@
+import { useAuth } from '@todocity/auth';
+import { UserAvatar } from '@todocity/components/user-avatar/user-avatar';
 import { Box, Heading, Text } from '@todocity/ui/core';
 
 export function AccountSettings() {
+  const { user } = useAuth();
+
   return (
     <Box>
       <Heading>Account</Heading>
-      <Text>Content</Text>
+      <UserAvatar />
+      <Text>Name: {user.displayName}</Text>
+      <Text>Email: {user.email}</Text>
     </Box>
   );
 }
