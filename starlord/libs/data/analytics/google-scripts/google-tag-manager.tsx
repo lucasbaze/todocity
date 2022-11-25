29fcf570d48f77ab6c07bb23f571656a04435d23
lucas <lucas.bazemore@republic.co>
2022-11-24 19:32:56 -0600

    Remove preview env for production

--- starlord/libs/data/analytics/google-scripts/google-tag-manager.tsx
+++ starlord/libs/data/analytics/google-scripts/google-tag-manager.tsx
@@ -9,7 +9,7 @@ export function GoogleTagManager() {
 					(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl+ '&gtm_auth=Dnoy0WCVydH2kPPgb4ZiLA&gtm_preview=env-1&gtm_cookies_win=x';f.parentNode.insertBefore(j,f);
+'https://www.googletagmanager.com/gtm.js?id='+i+dl+ '&gtm_cookies_win=x';f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_WEB_TAG_MANAGER_ID}')
 				`
         : `
